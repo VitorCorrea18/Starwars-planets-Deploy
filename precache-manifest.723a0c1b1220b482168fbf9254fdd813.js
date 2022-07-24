61165c6031bf290672e38a280aac582d7c4e7c64
Vitor Correa <vitorcorrea18@gmail.com>
2022-07-24 18:40:11 -0300

    Updates

---
+++ precache-manifest.723a0c1b1220b482168fbf9254fdd813.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae32a4cdd25dfed324c9563cf4c0c84b",
+    "url": "/starwars-planet-search/index.html"
+  },
+  {
+    "revision": "897e8679d95c14eb3c60",
+    "url": "/starwars-planet-search/static/css/main.6f137007.chunk.css"
+  },
+  {
+    "revision": "33d937040bf366772508",
+    "url": "/starwars-planet-search/static/js/2.8844a6dd.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/starwars-planet-search/static/js/2.8844a6dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "897e8679d95c14eb3c60",
+    "url": "/starwars-planet-search/static/js/main.fe0ebf63.chunk.js"
+  },
+  {
+    "revision": "7ae9ac9354f1d33a3553",
+    "url": "/starwars-planet-search/static/js/runtime-main.9d6297ae.js"
+  },
+  {
+    "revision": "928f42498d1eee368c3771f969143c89",
+    "url": "/starwars-planet-search/static/media/72155-space-stars-galaxy.928f4249.jpg"
+  }
+]);
